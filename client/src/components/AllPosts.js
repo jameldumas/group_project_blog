e73b940dec578fd1c704510ba9501f6da48ec0c0
jameldumas <80063985+jameldumas@users.noreply.github.com>
2021-06-25 23:02:56 -0400

    Update AllPosts.js

--- client/src/components/AllPosts.js
+++ client/src/components/AllPosts.js
@@ -1,10 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, navigate } from '@reach/router';
 
-const AllPosts = () => {
-    
+
+const AllPosts = (props) => {
+    const [ allPosts, setAllPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/posts')
+        .then((res) => {
+            console.log(res.data);
+            setAllPosts(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }, [])
+
     
     return(
-        <div></div>
+                <div>
+                    <h2></h2>
+                    <table>
+                        <tbody>
+                            {
+                                allPosts.map(( post, index) => (
+                                <tr>
+                                    <td  >
+                                        <p> 
+                                        <button onClick={() => navigate('/posts/' + post._id )}>
+                                            {post.title} 
+                                        </button> 
+                                            
+                                        </p>
+                                    </td>
+                                </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+      
     )
-} 
+}
+
 export default AllPosts;
