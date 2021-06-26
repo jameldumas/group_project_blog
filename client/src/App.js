bff28c29a4410cd6e8c746fbde716065cc2a74be
jameldumas <80063985+jameldumas@users.noreply.github.com>
2021-06-26 14:02:36 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import { Router } from '@reach/router';
+import { Link, navigate } from '@reach/router';
 import AllPosts from './components/AllPosts';
+import NewPost from './components/NewPost';
+import Login from './components/Login';
+import RegisterUser from './components/register';
+import Forbidden from './views/Forbidden';
+import LoginReg from './views/LoginReg';
+
+
+
 
 function App() {
   return (
     <div>
-      <h1>All Posts</h1>
+      <Router>
+        <AllPosts path="/blog/all_posts" /> 
+        <NewPost path="/blog/new_post" />
+        <Login path="/login" />
+        <RegisterUser path="/register" />
+        <Forbidden path="/forbidden_page" />
+        <LoginReg path="/login_register" />
+
+
+      </Router>
+        
     </div>
   )}
 
